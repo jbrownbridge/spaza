e39d29cd3375db8b8923a2a19dcb04066c5d81b2
Jason Brownbridge <jbrownbridge@gmail.com>
2011-07-11 15:55:43 +0200

    Changed serviceType from END to SESSION.

--- spaza/views.py
+++ spaza/views.py
@@ -4,7 +4,7 @@ def home(request):
 	returnMessageFormat = "msisdn=%s&message=%s&serviceType=%s"
 	msisdn      = request.REQUEST.get("msisdn", "no_cell_number")
 	message     = request.REQUEST.get("message", "no_message")
-	serviceType = "END" 
+	serviceType = "SESSION" 
 	
 	return HttpResponse(returnMessageFormat % (msisdn, message, serviceType))
 	
