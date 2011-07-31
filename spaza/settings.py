c1344f0242cb8eeb2d087e1e16f214c1c7b137db
Jason Brownbridge <jbrownbridge@gmail.com>
2011-07-31 23:43:07 +0200

    Updated settings.py
    Fixed timezone for server
    Made web an admin user

--- spaza/settings.py
+++ spaza/settings.py
@@ -18,6 +18,7 @@ MANAGERS = ADMINS
 ADMIN_USERNAMES = [
   'jason',
   'matt',
+  'web',
 ]
 
 DATABASES = {
@@ -38,7 +39,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Africa/Johannesburg'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
